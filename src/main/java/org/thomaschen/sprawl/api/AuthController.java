27ef956f718d94153912e9652a02e533c7a6ca9d
Thomas <thomas99k@gmail.com>
2019-04-15 02:53:20 -0400

    core - add CRUD api and login endpoint for Users

---
+++ src/main/java/org/thomaschen/sprawl/api/AuthController.java
@@ -0,0 +1,32 @@
+package org.thomaschen.sprawl.api;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.thomaschen.sprawl.exception.ResourceNotFoundException;
+import org.thomaschen.sprawl.model.UserDetail;
+import org.thomaschen.sprawl.repository.UserDetailRepository;
+
+import javax.validation.Valid;
+import java.security.Principal;
+import java.util.List;
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/api/auth")
+public class AuthController {
+
+    @Autowired
+    UserDetailRepository userDetailRepository;
+
+    // Get all UserDetails
+    @GetMapping("/login")
+    public ResponseEntity login(Principal principal) {
+        UserDetail userDetail = userDetailRepository.findByUsername(principal.getName());
+        if (userDetail == null) {
+            userDetailRepository.save(new UserDetail(principal.getName()));
+        }
+
+        return ResponseEntity.ok("Successfully Authenticated: " + principal.getName());
+    }
+}
